9baf2c1c92b41dca7fa23d6ed159bddddc21024b
Hugo Gomes <hugorgomes10@gmail.com>
2022-10-25 11:13:02 -0300

    created button event

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
-// 9e92702e2898916292e45b6533e6d2b0 KEY
+const apiKey = "9e92702e2898916292e45b6533e6d2b0";
+const apiCountryURL = "https://countryflagsapi.com/png/";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+const showWeatherData = (city) => {
+  console.log(city);
+};
+
+searchBtn.addEventListener("click", (a) => {
+  a.preventDefault();
+
+  const city = cityInput.value;
+
+  showWeatherData(city);
+});
