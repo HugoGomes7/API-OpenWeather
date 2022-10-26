e67a924fe0ca0f041d79c291356a7a947a025e80
Hugo Gomes <hugorgomes10@gmail.com>
2022-10-25 23:40:43 -0300

    api data selection

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,26 @@ const apiCountryURL = "https://countryflagsapi.com/png/";
 const cityInput = document.querySelector("#city-input");
 const searchBtn = document.querySelector("#search");
 
+const cityElement = document.querySelector("#city");
+const countryElement = document.querySelector("#country");
+const tempElement = document.querySelector("#temperature span");
+const descriptionElement = document.querySelector("#descriptions");
+const weatherIconElement = document.querySelector("#weather-icon");
+const umidityElement = document.querySelector("#umidity span");
+const windElement = document.querySelector("#wind span");
+
+
+const getWeatherData = async (city) => {
+  const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=en`;
+
+  const res = await fetch(apiWeatherURL);
+  const data = await res.json();
+
+  console.log(data);
+};
+
 const showWeatherData = (city) => {
-  console.log(city);
+  getWeatherData(city);
 };
 
 searchBtn.addEventListener("click", (a) => {
