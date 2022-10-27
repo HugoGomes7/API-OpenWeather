0711e25dbf81c008fe492a497f3fa46b7fff1834
Hugo Gomes <hugorgomes10@gmail.com>
2022-10-27 00:53:50 -0300

    last code review and finished project

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 const apiKey = "263e96693cdf4f0563b2b60d386367aa";
 const apiCountryURL = "https://countryflagsapi.com/png/";
+const apiUnsplash = "https://source.unsplash.com/1600x900/?";
 
 const cityInput = document.querySelector("#city-input");
 const searchBtn = document.querySelector("#search");
@@ -7,7 +8,7 @@ const searchBtn = document.querySelector("#search");
 const cityElement = document.querySelector("#city");
 const countryElement = document.querySelector("#country");
 const tempElement = document.querySelector("#temperature span");
-const descriptionElement = document.querySelector("#descriptions");
+const descElement = document.querySelector("#description");
 const weatherIconElement = document.querySelector("#weather-icon");
 const humidityElement = document.querySelector("#humidity span");
 const windElement = document.querySelector("#wind span");
@@ -17,8 +18,8 @@ const weatherContainer = document.querySelector("#weather-data");
 const errorMessageContainer = document.querySelector("#error-message");
 const loader = document.querySelector("#loader");
 
-const suggestionsContainer = document.querySelector("#suggestions");
-const suggestionsButton = document.querySelector("#suggestions button");
+const suggestionContainer = document.querySelector("#suggestions");
+const suggestionButtons = document.querySelectorAll("#suggestions button");
 
 const toggleLoader = () => {
   loader.classList.toggle("hide");
@@ -39,14 +40,14 @@ const getWeatherData = async (city) => {
 
 const showErrorMessage = () => {
   errorMessageContainer.classList.remove("hide");
-}
+};
 
 const hideInformation = () => {
   errorMessageContainer.classList.add("hide");
   weatherContainer.classList.add("hide");
 
-  suggestionsContainer.classList.add("hide");
-}
+  suggestionContainer.classList.add("hide");
+};
 
 const showWeatherData = async (city) => {
   hideInformation();
@@ -54,13 +55,13 @@ const showWeatherData = async (city) => {
   const data = await getWeatherData(city);
 
   if (data.cod === "404") {
-    showErrorMessage(data.message);
+    showErrorMessage();
     return;
   }
 
   cityElement.innerText = data.name;
   tempElement.innerText = parseInt(data.main.temp);
-  descriptionElement.innerText = data.weather[0].description;
+  descElement.innerText = data.weather[0].description;
   weatherIconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
@@ -90,9 +91,9 @@ cityInput.addEventListener("keyup", (a) => {
   }
 });
 
-suggestionsButtons.forEach((btn) => {
+suggestionButtons.forEach((btn) => {
   btn.addEventListener("click", () => {
-    const city = btn.getAtribute("id");
+    const city = btn.getAttribute("id");
 
     showWeatherData(city);
   });
